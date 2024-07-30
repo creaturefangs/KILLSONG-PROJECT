2687228d09cde17eaa5e88ada5256318806b76b9
creaturefangs <126419720+creaturefangs@users.noreply.github.com>
2024-07-30 18:56:19 -0400

    PUSHING MENU NAV

--- Assets/Scripts/UI/ButtonNav.cs
+++ Assets/Scripts/UI/ButtonNav.cs
@@ -2,10 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class ButtonNav : MonoBehaviour
 {
-   
+
+    public string sceneName; // The name of the scene to load
+    public Button loadSceneButton; // Reference to the button that triggers the scene load (optional)
+
+    private void Start()
+    {
+        // If a button is assigned, add the onClick listener
+        if (loadSceneButton != null)
+        {
+            loadSceneButton.onClick.AddListener(() => LoadSceneWithDelay(sceneName));
+        }
+    }
+
+    // Public method to be called to start the scene loading process
+    public void LoadSceneWithDelay(string sceneName)
+    {
+        StartCoroutine(LoadSceneAfterDelay(sceneName));
+    }
+
+    // Coroutine to handle the delay
+    private IEnumerator LoadSceneAfterDelay(string sceneName)
+    {
+        // Wait for 3 seconds
+        yield return new WaitForSeconds(3f);
+
+        // Load the scene
+        SceneManager.LoadScene(sceneName);
+    }
+
     public void OnQuitButtonClick()
     {
         Application.Quit();
@@ -41,4 +70,5 @@ public class ButtonNav : MonoBehaviour
         Application.OpenURL(URLname);
     }
 
+ 
 }
